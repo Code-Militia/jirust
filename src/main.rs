ef65dbbfdf64c51eeea8a118e99dc65a4cae83cc
Mo Ali <akali1987@gmail.com>
2023-01-22 19:15:15 -0500

    Final commit before changing to tui-realm

--- src/main.rs
+++ src/main.rs
@@ -91,6 +91,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     enable_raw_mode().expect("can run in raw mode");
     let (tx, rx) = mpsc::channel();
     let tick_rate = Duration::from_millis(200);
+    // TODO: Need to understand this better
     thread::spawn(move || {
         let mut last_tick = Instant::now();
         loop {
